c3c53ff611004662131fbb5499ad769ccc18fec4
Juliana Zhu <julianazhu89@gmail.com>
2015-03-03 11:23:21 +1100

    Moving Methods Fail

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -10,4 +10,25 @@ class Requirement < ActiveRecord::Base
   validates :amount, 
           presence: true,
           numericality: {only_integer: true}
+
+  def self.loop_through_story_requirements(story, character)
+    story.requirements.each do |requirement| 
+      corresponding_character_stat = character.stats.find_by(quality_id: requirement.quality.id) 
+      @requirement_eligibility = requirement_eligibility_calculator(requirement, corresponding_character_stat)
+      @story_eligibility = @story_eligibility && @requirement_eligibility
+      Story.add_story_to_eligible_stories_array(story) unless @story_eligibility == false
+    end
+  end
+  
+  def self.requirement_eligibility_calculator(requirement, stat)
+    if requirement.operation == "greater than"
+      stat.points > requirement.amount
+    elsif requirement.operation == "less than"
+      stat.points < requirement.amount
+    elsif requirment.operation == "equals"
+      stat.points == requirement.amount
+    else
+      raise "Error: Operation is not an accepted type. ABORT. FATAL. Ring the catastrophe bell."
+    end
+  end
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -10,4 +10,28 @@ class Story < ActiveRecord::Base
   validates :image_path, 
             allow_blank: true,
             length: { minimum: 3}
+         
+  def self.find_eligible_stories(character)
+    create_eligible_stories_array
+    loop_through_all_stories(character)
+  end 
+  
+  def self.create_eligible_stories_array
+    @eligible_stories = []
+  end
+  
+  def self.add_story_to_eligible_stories_array(story)
+    @eligible_stories = @eligible_stories.push(story)
+  end
+  
+  def self.loop_through_all_stories(character)
+    Story.all.each do |story|
+      @story_eligibility = true
+      if story.requirements.first.nil? 
+        @eligible_stories.push(story)
+      else
+        Requirement.loop_through_story_requirements(story, character)
+      end
+    end
+  end
 end
