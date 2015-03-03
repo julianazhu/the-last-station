512d0de3c6a1fa9703ee09e921d396bf37e4bb32
Juliana Zhu <julianazhu89@gmail.com>
2015-03-03 11:14:24 +1100

    Moved execute_effects

--- app/controllers/branches_controller.rb
+++ app/controllers/branches_controller.rb
@@ -11,35 +11,6 @@ before_action :get_all_qualities_and_stories, only: [:new, :edit, :create]
     @stories = Story.all
   end
   
-  def execute_effects
-    @outcomes = []
-    @branch.effects.each do |effect|
-      @stat = @character.stats.find_or_initialize_by(:quality_id => effect.quality.id) do |stat|
-        stat.points = 0
-      end
-      # Save temporary information about old stat points
-      @old_stat_points = @stat.points 
-      # Update the Character's stats per the operation
-      calculate_effect_result(effect, effect.operation)
-      @stat.save
-      outcome_summary = "Your #{effect.quality.name} has #{@operation_description} from #{@old_stat_points} to #{@stat.points}."
-      #Create the array of outcomes for the view
-      @outcomes.push(outcome_summary)
-    end
-  end
-  
-  def calculate_effect_result(effect, operation)
-    if operation == "plus"
-      @stat.points += effect.amount
-      @operation_description = "increased"
-    elsif operation == "minus"
-      @stat.points -= effect.amount
-      @operation_description = "decreased"
-    else
-      raise "Error: Operation is not an accepted value. ABORT. FATAL. Ring the catastrophe bell."
-    end
-  end
-  
   def index
     @branches = Branch.all
   end
@@ -47,7 +18,7 @@ before_action :get_all_qualities_and_stories, only: [:new, :edit, :create]
   def show
     @character = Character.find(params[:character_id])
     @character_id = params[:character_id].to_i
-    execute_effects unless @character.nil?
+    Branch.execute_effects(@branch, @character) unless @character.nil?
   end
 
   def new
--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,5 +1,4 @@
 class Branch < ActiveRecord::Base
-
   has_many :effects
   belongs_to :story
   validates :story_id, 
@@ -13,4 +12,33 @@ class Branch < ActiveRecord::Base
   validates :outcome, 
             presence: true,
             length: { minimum: 5}
+            
+  def self.execute_effects(branch, character)
+    @outcomes = []
+    branch.effects.each do |effect|
+      @stat = character.stats.find_or_initialize_by(:quality_id => effect.quality.id) do |stat|
+        stat.points = 0
+      end
+      # Save temporary information about old stat points
+      @old_stat_points = @stat.points 
+      # Update the Character's stats per the operation
+      calculate_effect_result(effect, effect.operation)
+      @stat.save
+      outcome_summary = "Your #{effect.quality.name} has #{@operation_description} from #{@old_stat_points} to #{@stat.points}."
+      #Create the array of outcomes for the view
+      @outcomes.push(outcome_summary)
+    end
+  end
+  
+  def self.calculate_effect_result(effect, operation)
+    if operation == "plus"
+      @stat.points += effect.amount
+      @operation_description = "increased"
+    elsif operation == "minus"
+      @stat.points -= effect.amount
+      @operation_description = "decreased"
+    else
+      raise "Error: Operation is not an accepted value. ABORT. FATAL. Ring the catastrophe bell."
+    end
+  end
 end
