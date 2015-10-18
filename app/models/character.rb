f98eca81758a0422660fe8d78950934737807ba6
jules <julianazhu89@gmail.com>
2015-10-18 22:58:00 +1100

    Prettied up the intro buttons

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,12 +3,12 @@ attr_writer :current_step
   has_many :stats, :dependent => :destroy
   validates :name, 
             :presence => { :message => "Pick a name! Any name! Ideally with letters and everything."},
-            format: { with: /\A[a-zA-Z\d ]*\Z/, :message => "You get a strong feeling that your name is made all out of letters. Maybe with spaces in between." },
+            format: { with: /\A[a-zA-Z\d ]*\Z/, :message => "You get a strong feeling that your name is made up of letters, and maybe spaces. Numbers probably indicate you have brain damage and/or bad parents, but that's not your fault." },
             :if => lambda { |f| f.current_step == "name" }
   validates :avatar_image_path,
             :presence => { :message =>  "It doesn't look like anything is selected...let's try this again. " },
             :if => lambda { |f| f.current_step == "avatar"}
-  validates_presence_of :gender, :if => lambda { |f| f.current_step == "gender"}, :message => "It's a difficult question, but here's a secret: you can just change your mind later. You have that power. So just pick something."
+  validates_presence_of :gender, :if => lambda { |f| f.current_step == "gender"}, :message => "It's a difficult question, but there's a secret: you can just change your mind later. So just pick anything."
 
 # Related to the Introduction Form wizard
 def current_step
